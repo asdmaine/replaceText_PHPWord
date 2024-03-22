0bdfb0089a7c8de5c6ca599d07223402fa7db99a
sulthon1121 <sulthon.sdn@gmail.com>
2024-03-22 12:37:44 +0700

    first

---
+++ application/views/edit.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'vendor/autoload.php';
+
+$templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor('src/docx/QSF HRD 041 - Gate Pass Permit.docx');
+$templateProcessor->setValue('date', 'ini tanggal');
+$templateProcessor->setValue('penjelasan', 'PENJELASAN NYA DISINI');
+
+// style checkbox
+$CheckedBox='<w:sym w:font="Wingdings" w:char="F0FE"/>';
+$unCheckedBox = '<w:sym w:font="Wingdings" w:char="F0A8"/>';
+
+$templateProcessor->setValue('urusan_perusahaan', $CheckedBox);
+$templateProcessor->setValue('urusan_keluarga', $unCheckedBox);
+$templateProcessor->setValue('sakit', $unCheckedBox);
+$templateProcessor->setValue('lain-lain', $unCheckedBox);
+
+
+$templateProcessor->saveAs('./src/docx/output.docx');
